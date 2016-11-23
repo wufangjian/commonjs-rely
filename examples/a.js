6b42883efa92a8456058041ad1c4296f106f9c66
wufangjian <wufangjian@baidu.com>
2016-11-23 11:39:32 +0800

    update

---
+++ examples/a.js
@@ -0,0 +1,7 @@
+define('a', function(module, exports, rely) {
+  function print() {
+    return '~ hello ~ rely!';
+  }
+
+  exports.print = print;
+});
--- rely.js
+++ rely.js
@@ -1,56 +1,54 @@
+var rely, define;
+(function() {
+    var list = {};
 
+    // 模块注册
+    define = function(path, fn) {
+        list[path] = fn;
+    };
 
-rely.modules = {};
+    // 模块引用
+    rely = function(mid) {
+        var path = getPath(mid);
+        var mod = list[path];
 
-// 注册模块
-rely.define = function(path, fn) {
-  rely.modules[path] = fn;
-}
+        if (!mod) {
+            throw new Error('failed to rely "' + mid + '"');
+        };
+        if (!mod.exports) {
+            mod.exports = {};
+            mod.apply(mod.exports, [mod, mod.exports, relative(path)]);
+        }
+        return mod.exports;
+    };
 
-// 获取模块
-rely.getPath = function(mid) {
-  var orig = mid;
-  var reg = mid + '.js';
-  var index = mid + '/index.js';
-  return rely.modules[reg] && reg || rely.modules[index] && index || orig;
-}
-
-// 
-rely.relative = function(parent) {
-  return function(mid) {
-    if ('.' != mid.charAt(0)) {
-      return rely(mid);
+    // 获取模块id
+    function getPath(mid) {
+        var orig = mid;
+        var reg = mid + '.js';
+        var index = mid + '/index.js';
+        return list[reg] && reg || list[index] && index || orig;
     }
 
-    var path = parent.split('/');
-    var segs = mid.split('/');
-    path.pop();
+    function relative(parent) {
+        return function(mid) {
+            if ('.' != mid.charAt(0)) {
+                return rely(mid);
+            }
 
-    for (var i = 0; i < segs.length; i++) {
-      var seg = segs[i];
-      if (seg == '..') {
-        path.pop();
-      } else if (seg != '.') {
-        path.push(seg);
-      }
+            var path = parent.split('/');
+            var segs = mid.split('/');
+            path.pop();
+
+            for (var i = 0; i < segs.length; i++) {
+                var seg = segs[i];
+                if (seg == '..') {
+                    path.pop();
+                } else if (seg != '.') {
+                    path.push(seg);
+                }
+            }
+            return rely(path.join('/'));
+        }
     }
-
-    return rely(path.join('/'));
-  }
-}
-
-// 引用模块
-function rely(mid) {
-  var path = rely.getPath(mid);
-  var mod = rely.modules[path];
-  if (!mod) {
-    throw new Error('failed to rely "' + mid + '"');
-  };
-
-  if (!mod.exports) {
-    mod.exports = {};
-    mod.call(mod.exports, mod, mod.exports, rely.relative(path));
-  }
-
-  return mod.exports;
-}
+})();
